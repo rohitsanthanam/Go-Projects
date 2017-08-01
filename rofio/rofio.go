a6e74e1b2b7a093599cecd1585b37de09fea990e
rohitsanthanam <30610342+rohitsanthanam@users.noreply.github.com>
2017-08-01 06:28:24 -0500

    Comments
    Added explanatory comments.

--- rofio/rofio.go
+++ rofio/rofio.go
@@ -6,6 +6,10 @@ import (
 
 type FILE_OPEN_FUNC func (string) (*os.File, error)
 
+// This internal file open function opens a file using the specified
+// filename and specific open function from the OS package.
+// It also returns an anonymous function for closing the file which is
+// meant to be deferred at the call site.
 func open_file (f_name string, f_func FILE_OPEN_FUNC) (*os.File, func()) {
 	fi, err := f_func (f_name)
 	if (err != nil) {
@@ -14,10 +18,12 @@ func open_file (f_name string, f_func FILE_OPEN_FUNC) (*os.File, func()) {
 	return fi, func() { if err := fi.Close(); err != nil { panic (err) } }
 }
 
+// Open an existing file.
 func Open_file (file_name string) (*os.File, func()) {
 	return open_file (file_name, os.Open)
 }
 
+// Create a new file.
 func Create_file (file_name string) (*os.File, func()) {
 	return open_file (file_name, os.Create)
 }
